0891aa158d12a635337d7b9a5fa2cee5711ff8ec
wrendev <wrenfajardo123@gmail.com>
2020-09-05 18:00:29 +1000

    add google map

--- frontend2/src/Map/Maps.js
+++ frontend2/src/Map/Maps.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import "../App.css";
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper } from "google-maps-react";
 
+const mapStyles = {
+  width: "100%",
+  height: "100%",
+};
 
 class Maps extends Component {
   constructor(props) {
@@ -10,8 +14,17 @@ class Maps extends Component {
   }
 
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176 }}
+        />
+      </div>
+    );
   }
 }
 
-export default Maps;
+export default GoogleApiWrapper({apiKey: "AIzaSyB9txFFascb8Jcj8qV6ET2mtXZtwqqzMiU",})(Maps);
