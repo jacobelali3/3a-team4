4ad4beb1b8c6ffd8a548cdc6b6b573f671358468
jacobelali3 <fromtwoislands@hotmail.com>
2020-09-07 03:54:45 +1000

    optimised results, added css

--- frontend2/src/news.js
+++ frontend2/src/news.js
@@ -1,12 +1,16 @@
 
 var url = 'http://cors-anywhere.herokuapp.com/http://newsapi.org/v2/everything?' +
           'q=covid 19&' +
+           'q=australia' +
+           'qInTitle=covid' +
           'from=2020-09-06&' +
-          'sortBy=popularity&' +
+          'sortBy=relevancy&' +
           'apiKey=93fa47f73b0544e2a927b0a0d8d6db98';
 
 var req = new Request(url);
 const newsList = document.querySelector('.news-list');
+
+
 fetch(req)
     .then((response)=> {return response.json()}).then((data)=>{console.log(data)
        data.articles.forEach(article =>{
@@ -17,6 +21,12 @@ fetch(req)
            a.textContent = article.title;
            li.appendChild(a);
            newsList.appendChild(li);
+           /*var img = new Image(); 
+           img.setAttribute('href', article.url);
+            img.src =  article.urlToImage; 
+            document.getElementById('body').appendChild(img); */
+
+           
         })
             
         });
