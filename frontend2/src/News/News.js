9a9bb250ee2df9cc0129da0db84cc5d78508b9e0
Maccas21 <mcmouny.heng@live.com>
2020-09-07 14:20:32 +1000

    news.js didnt commit before

--- frontend2/src/News/News.js
+++ frontend2/src/News/News.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+import Card from './Card'
 import "../App.css";
+
+import Grid from '@material-ui/core/Grid';
 
 class News extends Component {
   constructor(props) {
@@ -8,7 +11,33 @@ class News extends Component {
   }
 
   render() {
-    return <div>News</div>;
+    return (
+      <div>News
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card/>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card/>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card/>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card/>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card/>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card/>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card/>
+          </Grid>
+        </Grid>
+      </div>
+    );
   }
 }
 
