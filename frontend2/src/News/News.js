6e13fa479b82685374e6f54d2d130660a11e249e
Maccas21 <mcmouny.heng@live.com>
2020-09-27 23:37:15 +1000

    cards now get data from the news api

--- frontend2/src/News/News.js
+++ frontend2/src/News/News.js
@@ -12,62 +12,12 @@ import {
 	CardActionArea,
 	CardMedia,
 	CardContent,
-	Link,
 	Typography,
-	CardHeader,
 } from "@material-ui/core";
 class News extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			cardItems: [
-				{
-					id: 1,
-					title: "News Article",
-					img:
-						"https://images.newscientist.com/wp-content/uploads/2020/02/11165812/c0481846-wuhan_novel_coronavirus_illustration-spl.jpg",
-					description:
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-					link: "http://www.google.com",
-				},
-				{
-					id: 2,
-					title: "2News Article",
-					img:
-						"https://images.newscientist.com/wp-content/uploads/2020/02/11165812/c0481846-wuhan_novel_coronavirus_illustration-spl.jpg",
-					description:
-						"2Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-					link: "http://www.yahoo.com",
-				},
-				{
-					id: 3,
-					title: "3News Article",
-					img:
-						"https://images.newscientist.com/wp-content/uploads/2020/02/11165812/c0481846-wuhan_novel_coronavirus_illustration-spl.jpg",
-					description:
-						"3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-					link: "http://www.twitter.com",
-				},
-				{
-					id: 4,
-					title: "4News Article",
-					img:
-						"https://images.newscientist.com/wp-content/uploads/2020/02/11165812/c0481846-wuhan_novel_coronavirus_illustration-spl.jpg",
-					description:
-						"4Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-					link: "http://www.bing.com",
-				},
-				{
-					id: 5,
-					title: "5News Article",
-					img:
-						"https://images.newscientist.com/wp-content/uploads/2020/02/11165812/c0481846-wuhan_novel_coronavirus_illustration-spl.jpg",
-					description:
-						"5Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-					link: "https://online.uts.edu.au/webapps/login/",
-				},
-			],
-		};
+		this.state = { cardItems: [] };
 	}
 
 	componentDidMount() {
@@ -84,7 +34,7 @@ class News extends Component {
 			"language=en&" +
 			"apiKey=93fa47f73b0544e2a927b0a0d8d6db98";
 		const req = new Request(url);
-		const newsList = document.querySelector(".news-list");
+		// const newsList = document.querySelector(".news-list");
 		fetch(req)
 			.then((response) => {
 				return response.json();
@@ -108,14 +58,23 @@ class News extends Component {
 					c.textContent = article.description;
 					li.appendChild(a);
 
-					newsList.appendChild(li);
+					/* newsList.appendChild(li);
 					newsList.appendChild(b);
 					newsList.appendChild(c);
-					newsList.appendChild(d);
+					newsList.appendChild(d); */
 					/*var img = new Image(); 
-           img.setAttribute('href', article.url);
-            img.src =  article.urlToImage; 
-            document.getElementById('body').appendChild(img); */
+           			img.setAttribute('href', article.url);
+            		img.src =  article.urlToImage; 
+					document.getElementById('body').appendChild(img); */
+
+					var tempCards = this.state.cardItems.concat({
+						title: article.title,
+						img: article.urlToImage,
+						description: article.description,
+						link: article.url,
+					});
+
+					this.setState({ cardItems: tempCards });
 				});
 			});
 	}
@@ -127,46 +86,63 @@ class News extends Component {
 					<h1>Today's COVID News </h1>
 				</div>
 
-				<div class="container">
+				{/* <div class="container">
 					<ul class="news-list"></ul>
+				</div> */}
 
-					<Grid container spacing={4} style={{ padding: 24 }}>
-						{this.state.cardItems.map((item, index) => (
-							<Grid item xs={12} sm={6} md={4}>
-								<Card style={{ minWidth: 285 }}>
-									<CardActionArea onClick={() => window.open(item.link)}>
-										<CardMedia
-											componenet="img"
-											image={item.img}
-											title={item.title}
-											style={{ height: 0, paddingTop: "50%" }}
-										/>
-										<CardContent>
-											<Typography gutterBottom variant="h5" component="h2">
-												{item.title}
-											</Typography>
-											<Typography
-												variant="body2"
-												color="textSecondary"
-												component="p"
-											>
-												{item.description}
-											</Typography>
-										</CardContent>
-									</CardActionArea>
-									<CardActions>
-										<Button size="small" color="primary">
-											Share
-										</Button>
-										<Button size="small" color="primary">
-											Learn More
-										</Button>
-									</CardActions>
-								</Card>
-							</Grid>
-						))}
-					</Grid>
-				</div>
+				<Grid container spacing={4} style={{ padding: 24 }}>
+					{this.state.cardItems.map((item, index) => (
+						<Grid item xs={12} sm={6} md={4} xl={3}>
+							<Card style={{ minWidth: 285 }}>
+								<CardActionArea onClick={() => window.open(item.link)}>
+									<CardMedia
+										componenet="img"
+										image={item.img}
+										title={item.title}
+										style={{ height: 0, paddingTop: "50%" }}
+									/>
+									<CardContent>
+										<Typography
+											gutterBottom
+											variant="h5"
+											component="h2"
+											style={{
+												overflow: "hidden",
+												display: "-webkit-box",
+												WebkitLineClamp: 2,
+												WebkitBoxOrient: "vertical",
+											}}
+										>
+											{item.title}
+										</Typography>
+										<Typography
+											variant="body2"
+											color="textSecondary"
+											component="p"
+											style={{
+												overflow: "hidden",
+												display: "-webkit-box",
+												WebkitLineClamp: 3,
+												WebkitBoxOrient: "vertical",
+											}}
+										>
+											{item.description}
+										</Typography>
+									</CardContent>
+								</CardActionArea>
+								<CardActions>
+									<Button
+										size="small"
+										color="primary"
+										onClick={() => window.open(item.link)}
+									>
+										Learn More
+									</Button>
+								</CardActions>
+							</Card>
+						</Grid>
+					))}
+				</Grid>
 			</div>
 		);
 	}
