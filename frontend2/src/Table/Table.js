deb8acbd34f27566e5d4f4f8f79fa912d1acfbf2
jacobelali3 <fromtwoislands@hotmail.com>
2020-10-19 20:41:25 +1100

    Removed duplicate news articles

--- frontend2/src/Table/Table.js
+++ frontend2/src/Table/Table.js
@@ -1,36 +1,40 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { Component, useMemo} from "react";
 import "../App.css";
-import moment from "moment";
+import { useTable } from 'react-table';
+import moment from 'moment';
 
 
 class Table extends Component {
     constructor(props) {
         super(props);
-        this.state = { tableList: [] };
+        this.state = {
+            data: [],
+        };
+        
   }
   
-
+  
+ 
   componentDidMount() {
-    var date = moment().subtract(10, "days").format("YYYY-MM-DD");
+    var date = moment().subtract(2, "days").format("YYYY-MM-DD");
     var dateCurr = moment().format("YYYY-MM-DD");
     const url = "https://api.covid19api.com/country/australia/status/confirmed?from=" + date + 
     "T00:00:00Z&to=" + dateCurr + "T00:00:00Z";
     const req = new Request(url);
     fetch(req)
-    .then((response)=> {return response.json()}).then((data)=>{console.log(data);
-       
-        })
-            
-        
-  
-}
+    .then((response)=> {return response.json()}).then((data)=>{
+        console.log(data);
+    this.setState({data: data})
+    })
+    
+ 
+  }
     render(){
-       return(
-           
-               <h1>Table</h1>
-        
-       );
-      }
+     return(  
+       <div></div>
+    )
+  }
+ 
+       
 }
-
 export default Table;
